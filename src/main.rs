b97bcbae8fc4869357b31ecab8b178be4b6832ce
GHexxerBrdv <gaurang1415@gmail.com>
2026-02-17 17:59:26 +0530

    Case update

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,32 @@
 #[derive(Debug)]
 enum RlpItem {
-    String(Vec<u8>)
+    String(Vec<u8>),
 }
 
-struct encoder;
+struct Encoder;
 
-impl encoder {
-    
+impl Encoder {
     fn new() -> Self {
         Self
     }
-    
+
     fn encode(&self, item: &RlpItem) -> Vec<u8> {
         match item {
-            RlpItem::String(bytes) => self.encode_string(bytes)
+            RlpItem::String(bytes) => self.encode_string(bytes),
         }
     }
-    
+
     fn encode_string(&self, bytes: &[u8]) -> Vec<u8> {
         match bytes.len() {
             1 if bytes[0] <= 0x7f => vec![bytes[0]],
-            
+
             len if len <= 55 => {
                 let mut encoded = Vec::with_capacity(1 + len);
                 encoded.push(0x80 + len as u8);
                 encoded.extend_from_slice(bytes);
                 encoded
             }
-            
+
             len => {
                 let len_bytes = len.to_be_bytes();
                 let len_bytes_trimmed = self.trim_leading_zeros(&len_bytes);
@@ -38,10 +37,9 @@ impl encoder {
                 encoded.extend_from_slice(bytes);
                 encoded
             }
-            
         }
     }
-    
+
     fn trim_leading_zeros<'a>(&self, bytes: &'a [u8; 8]) -> &'a [u8] {
         let mut start = 0;
         while start < bytes.len() && bytes[start] == 0 {
@@ -56,10 +54,10 @@ impl encoder {
 }
 
 fn main() {
-    let enc = encoder::new();
-    
+    let encoder = Encoder::new();
+
     let item = RlpItem::String(b"dog".to_vec());
-    let encoded = enc.encode(&item);
-    
+    let encoded = encoder.encode(&item);
+
     println!("Encoded dog: 0x{:02x?}", encoded);
 }
