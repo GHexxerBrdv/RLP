3f211c2ba9b663ee693e0301c5d084e14991df54
GHexxerBrdv <gaurang1415@gmail.com>
2026-02-18 12:55:38 +0530

    - add list encoding
    - refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 enum RlpItem {
     String(Vec<u8>),
+    List(Vec<RlpItem>),
 }
 
 struct Encoder;
@@ -13,23 +14,26 @@ impl Encoder {
     fn encode(&self, item: &RlpItem) -> Vec<u8> {
         match item {
             RlpItem::String(bytes) => self.encode_string(bytes),
+            RlpItem::List(items) => self.encode_list(items),
         }
     }
 
     fn encode_string(&self, bytes: &[u8]) -> Vec<u8> {
-        match bytes.len() {
-            1 if bytes[0] <= 0x7f => vec![bytes[0]],
+        let len = bytes.len();
+        match len {
+            0 => vec![0x80],
 
-            len if len <= 55 => {
-                let mut encoded = Vec::with_capacity(1 + len);
-                encoded.push(0x80 + len as u8);
+            1 if bytes[0] < 0x80 => vec![bytes[0]],
+
+            len if len < 56 => {
+                let mut encoded = vec![0x80 + len as u8];
                 encoded.extend_from_slice(bytes);
                 encoded
             }
 
             len => {
                 let len_bytes = len.to_be_bytes();
-                let len_bytes_trimmed = self.trim_leading_zeros(&len_bytes);
+                let len_bytes_trimmed = Self::trim_leading_zeros(&len_bytes);
                 let len_of_len = len_bytes_trimmed.len();
                 let mut encoded = Vec::with_capacity(1 + len_of_len + len);
                 encoded.push(0xb7 + len_of_len as u8);
@@ -40,7 +44,38 @@ impl Encoder {
         }
     }
 
-    fn trim_leading_zeros<'a>(&self, bytes: &'a [u8; 8]) -> &'a [u8] {
+    fn encode_list(&self, items: &[RlpItem]) -> Vec<u8> {
+        let mut encoded_items = Vec::new();
+
+        // encode all items in the list
+        for item in items {
+            encoded_items.extend(self.encode(item));
+        }
+
+        let payload_len = encoded_items.len();
+
+        match payload_len {
+            len if len < 56 => {
+                let mut encoded = Vec::with_capacity(1 + len);
+                encoded.push(0xc0 + len as u8);
+                encoded.extend(encoded_items);
+                encoded
+            }
+            len => {
+                let len_bytes = len.to_be_bytes();
+                let len_bytes_trimmed = Self::trim_leading_zeros(&len_bytes);
+                let len_of_len = len_bytes_trimmed.len();
+
+                let mut encoded = Vec::with_capacity(1 + len_of_len + len);
+                encoded.push(0xf7 + len_of_len as u8);
+                encoded.extend_from_slice(len_bytes_trimmed);
+                encoded.extend(encoded_items);
+                encoded
+            }
+        }
+    }
+
+    fn trim_leading_zeros(bytes: &[u8]) -> &[u8] {
         let mut start = 0;
         while start < bytes.len() && bytes[start] == 0 {
             start += 1;
@@ -58,6 +93,19 @@ fn main() {
 
     let item = RlpItem::String(b"dog".to_vec());
     let encoded = encoder.encode(&item);
+    println!("Encoded dog: 0x{}", hex::encode(encoded));
 
-    println!("Encoded dog: 0x{:02x?}", encoded);
+    let item = RlpItem::List(vec![
+        RlpItem::String(b"dog".to_vec()),
+        RlpItem::String(b"cat".to_vec()),
+        RlpItem::List(vec![
+            RlpItem::String(b"rabbit".to_vec()),
+            RlpItem::String(b"hamster".to_vec()),
+        ]),
+    ]);
+    let encoded = encoder.encode(&item);
+    println!(
+        "Encoded ['dog', 'cat', ['rabbit', 'hamster']]: 0x{}",
+        hex::encode(encoded)
+    );
 }
